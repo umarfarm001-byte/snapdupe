e988ea4ff1f0da10f5b16e11af4c65fed7897ea0
umarfarm001-byte <umar.farm001@gmail.com>
2026-03-28 21:21:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const AFFILIATE_CONFIG = {
+  amazon: {
+    tag: "YOUR_AMAZON_TAG-21",
+    baseUrl: "https://www.amazon.in/s",
+    active: false
+  },
+  nykaa: {
+    tag: "YOUR_NYKAA_TAG",
+    baseUrl: "https://www.nykaa.com/search/result/",
+    active: false
+  }
+};
